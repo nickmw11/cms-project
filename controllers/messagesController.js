f14482c33ddaa7c3accd249e814bc0c8ae55c555
John Paul Depew <jpdepewjp@gmail.com>
2018-04-24 20:27:08 -0400

    Added messages display file

--- controllers/messagesController.js
+++ controllers/messagesController.js
@@ -5,21 +5,21 @@
  */
 
 // mysql connection
-var configDB = require('../config/database.js');
+var mysqlConnect = require('../config/database.js');
 
 exports.displayMessages = function(req, res){
-  var query = "Select * from contactus"
-	var resultString = "";
-	var divStartString = "<div class=\"bg-dark\">";
+  var query = "SELECT * from contactus"
+	var messageArray = [];
 
-	configDB.query(query, function (err, result, fields) {
+	mysqlConnect.query(query, function (err, result, fields) {
 	  if (err) throw err;
 
 	  numRows = result.length;
-	  var articleArray = [];
 	  for (i = numRows - 1; i >= 0; i--) {
-	  	resultString = resultString + divStartString + "<h2>Name: " + result[i].firstname + " " + result[i].lastname + "</h2><h4>" + "Email: " + result[i].email + "</h4><h4>Phone:" + result[i].phone + "</h4><h2>" + result[i].subjectofmessage + "</h2><p>" + result[i].bodyofmessage + "</p></div>";
-	}
-	  res.send(resultString);
+			messageArray.push({ firstname: result[i].firstname, lastname: result[i].lastname, email: result[i].email, phone: result[i].phone, subject: result[i].subjectofmessage, body: result[i].bodyofmessage })
+		}
+		res.render('displays/messagesDisplay', {
+			messageArray: messageArray
+		});
   });
 }
